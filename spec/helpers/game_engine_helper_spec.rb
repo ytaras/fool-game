a11b118ca7ba43196d15c944b6c509788d3bea37
Yura Taras <yura.taras@gmail.com>
2012-05-31 19:28:32 +0300

    Spork

--- spec/helpers/game_engine_helper_spec.rb
+++ spec/helpers/game_engine_helper_spec.rb
@@ -1,18 +1,11 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the GameEngineHelper. For example:
-#
-# describe GameEngineHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe GameEngineHelper do
   pending "add some examples to (or delete) #{__FILE__}"
   describe "deck creation" do
   	pending "Add some examples to deck creation"
+  end
+  it "should answer" do
+  	helper.answer.should == 42
   end
 end
