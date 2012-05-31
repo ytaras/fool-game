fed1235cfe9aafd9f0b66a56776b47c94c67cedd
Yura Taras <yura.taras@gmail.com>
2012-05-31 23:01:25 +0300

    Players draw cards

--- app/helpers/game_engine_helper.rb
+++ app/helpers/game_engine_helper.rb
@@ -6,7 +6,7 @@ module GameEngineHelper
 	Card = Struct.new(:suit, :card)
 
 	def create_game
-		Game.new(sorted_deck.shuffle)
+		Game.new(sorted_deck.shuffle!)
 	end
 
 	def sorted_deck
@@ -16,10 +16,27 @@ module GameEngineHelper
 	end
 
 	class Game
-		attr_reader :deck_cards
+		attr_reader :deck_cards, :player1_cards, :player2_cards
 
 		def initialize(starting_deck)
 			@deck_cards = starting_deck.to_a
+			@player1_cards = []
+			@player2_cards = []
+		end
+
+		def next_move
+			draw_cards(:player1)
+			draw_cards(:player2)
+		end
+
+		private 
+
+		def draw_cards(player)
+			player_cards = send(player.to_s + "_cards")
+			cards_to_draw = 6 - player_cards.size
+			if(cards_to_draw > 0)
+				deck_cards.pop(cards_to_draw).each { |it| player_cards.push(it) }
+			end
 		end
 	end
 end
--- spec/helpers/game_engine_helper_spec.rb
+++ spec/helpers/game_engine_helper_spec.rb
@@ -1,12 +1,29 @@
 require 'spec_helper'
 
 describe GameEngineHelper do
+	before(:each) do
+		@game = helper.create_game
+	end
   describe "new game" do
-  	it "deck contains all cards" do
-  		game = helper.create_game
-  		game.should have(36).deck_cards
-  		game.deck_cards.should =~ helper.sorted_deck
-  		game.deck_cards.should_not == helper.sorted_deck
+  	describe "deck" do
+	  	it "deck contains all cards" do
+	  		@game.should have(36).deck_cards
+	  		@game.deck_cards.should =~ helper.sorted_deck
+	  		@game.deck_cards.should_not == helper.sorted_deck
+	  	end
+	  end
+
+  	describe "players" do
+  		it "not have cards on start" do
+  			@game.should have(0).player1_cards
+  			@game.should have(0).player2_cards
+  		end
+
+  		it "have 6 cards each after draw" do
+  			@game.next_move
+  			@game.should have(6).player1_cards
+  			@game.should have(6).player2_cards
+  		end
   	end
   end
 end
