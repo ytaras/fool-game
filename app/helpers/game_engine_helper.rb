d1e32137c105460b2d460884e47460747ab2ccad
Yura Taras <yura.taras@gmail.com>
2012-05-31 23:36:57 +0300

    Determine first move

--- app/helpers/game_engine_helper.rb
+++ app/helpers/game_engine_helper.rb
@@ -5,10 +5,12 @@ module GameEngineHelper
 	
 	Card = Struct.new(:suit, :card)
 
-	def create_game
-		Game.new(sorted_deck.shuffle!)
+	def create_game(starting_deck = nil)
+		starting_deck = sorted_deck.shuffle unless starting_deck
+		Game.new(starting_deck)
 	end
 
+	# TODO sorted deck should be created only once
 	def sorted_deck
 		cards = Array.new
 		SUITS.each { |suit| CARDS.each { |card| cards.push Card.new(suit, card) }}
@@ -16,6 +18,7 @@ module GameEngineHelper
 	end
 
 	class Game
+		attr_accessor :current_move
 		attr_reader :deck_cards, :player1_cards, :player2_cards, :trump
 
 		def initialize(starting_deck)
@@ -28,9 +31,28 @@ module GameEngineHelper
 		def next_move
 			draw_cards(:player1)
 			draw_cards(:player2)
+			if(@current_move.nil?)
+				p1_trump = smallest_trump(:player1)
+				p2_trump = smallest_trump(:player2)
+				if(p1_trump.nil?)
+					@current_move = :player2
+				elsif p1_trump.nil?
+			 		@current_move = :player1
+			 	elsif p1_trump < p2_trump
+			 		@current_move = :player1
+				else
+					@current_move = :player2
+				end
+			else
+			end
 		end
 
 		private 
+
+		def smallest_trump(player)
+			player_cards = send(player.to_s + "_cards")
+			player_cards.select { |it| it.suit == @trump }.map { |it| it.card }.min_by { |it| CARDS.find_index(it) }
+		end
 
 		def draw_cards(player)
 			player_cards = send(player.to_s + "_cards")
--- spec/helpers/game_engine_helper_spec.rb
+++ spec/helpers/game_engine_helper_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe GameEngineHelper do
+
 	before(:each) do
 		@game = helper.create_game
 	end
-  describe "new game" do
   	describe "deck" do
 	  	it "deck contains all cards" do
 	  		@game.should have(36).deck_cards
@@ -15,19 +15,29 @@ describe GameEngineHelper do
 	  	it "knows the trump" do
 	  		@game.trump.should == @game.deck_cards.last.suit
 	  	end
-	  end
+	end
+
+	describe "game" do
+		it "gives first turn to a gamer with lowest trump" do
+			start_deck = GameEngineHelper::CARDS.map {|it| GameEngineHelper::Card.new(:heart, it)}
+			@game = helper.create_game(start_deck)
+			@game.next_move
+			@game.trump.should == :heart
+			@game.player2_cards.map { |it| it.card }.should include("6")
+			@game.current_move.should == :player2
+		end
+	end
 
   	describe "players" do
-  		it "not have cards on start" do
-  			@game.should have(0).player1_cards
-  			@game.should have(0).player2_cards
-  		end
+		it "not have cards on start" do
+			@game.should have(0).player1_cards
+			@game.should have(0).player2_cards
+		end
 
-  		it "have 6 cards each after draw" do
-  			@game.next_move
-  			@game.should have(6).player1_cards
-  			@game.should have(6).player2_cards
-  		end
-  	end
-  end
+		it "have 6 cards each after draw" do
+			@game.next_move
+			@game.should have(6).player1_cards
+			@game.should have(6).player2_cards
+		end
+	end
 end
