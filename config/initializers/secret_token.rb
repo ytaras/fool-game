662d9b0fb872ef6b9e27abfdc4d362a756415c65
Yura Taras <yura.taras@gmail.com>
2012-05-31 15:00:02 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoolGame::Application.config.secret_token = 'c95ad0941764807eae0fb64ba98438124e83fcc23fcfe65cca9c9c8f7cba219468fa59188673f74b03b4b69890c9d0d677c8f6037bec2b2e3666553d8496834b'
