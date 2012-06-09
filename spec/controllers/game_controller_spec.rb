c92126bd4d5507075ff0a27f17b880df2a484698
Yura Taras <yura.taras@gmail.com>
2012-06-09 10:26:58 +0300

    Game cotroller spec

--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe GameController do
-
+  shared_examples "protected" do |method|
+    it "redirects to login" do
+      get method
+      response.should redirect_to new_user_session_path
+    end
+  end
+  describe :create do
+    include_examples "protected", :create
+  end
 end
